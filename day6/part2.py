069804d225ac98ee7bcd12430bf632035daecc98
Henrik O. Skogmo <henrik@skogmo.me>
2020-12-06 06:38:19 +0000

    Solves day6 part2

--- day6/part2.py
+++ day6/part2.py
@@ -2,51 +2,51 @@
 def read_declaration_forms(file_path):
     groups = []
     with open(file_path, 'r') as file:
-        # each person answers in the group is added as a set to a list
-        people_answers = []
+        new_group = Group()
+        # each line contains the answers from a single individual
         # file.read().splitlines() removes "\n" from the end of the lines
         for line in file.read().splitlines():
             person_answers = set()
             # if the line is empty then we start reading a new groups answers
             if not line.strip():
-                # look through people_answers, extract the common ones from all the answer sets
-                # todo: that ^^
-
-                print(f'people answers: {people_answers}')
-                common_answers = set()
-                print(f'Adding group with answers ...')
-                groups.append(Group(common_answers))
-                people_answers = []
+                print(f'Adding group with answers ... {new_group.people_answers}')
+                groups.append(new_group)
+                new_group = Group()
                 continue
 
-            # add each char as an answer to the set
+            # add each char as an answer to the set for the person
             # sets cannot contain duplicates, so they'll be discarded
             for char in line:
                 person_answers.add(char)
 
-            people_answers.append(person_answers)
+            new_group.people_answers.append(person_answers)
 
-        print(f'{person_answers}')
-        common_answers = set()
-        common_answers = person_answers
-        groups.append(Group(common_answers))
+        groups.append(new_group)
 
     return groups
 
 
 # Is a class really necessary? No, not really. But I kinda like it :)
 class Group:
-    def __init__(self, answers=None):
-        if answers is None:
-            answers = set()
-        self.answers = answers
+    def __init__(self, people_answers=None):
+        if people_answers is None:
+            people_answers = list()
+        self.people_answers = people_answers
+
+    def get_unanimous_yes_answers(self):
+        # take the first set as starting point
+        unanimous = self.people_answers[0]
+        # find the intersection of all following sets
+        for answers in self.people_answers[1:]:
+            unanimous = unanimous.intersection(answers)
+        return unanimous
 
 
-groups = read_declaration_forms('input-test.txt')
+groups = read_declaration_forms('input.txt')
 
-yes_answer_count = 0
+unanimous_yes_answer_count = 0
 for group in groups:
-    print(f'Group answered: {group.answers}, total {len(group.answers)} answers')
-    yes_answer_count += len(group.answers)
+    print(f'Group answers per individual: {group.people_answers}, unanimous yes: {group.get_unanimous_yes_answers()}')
+    unanimous_yes_answer_count += len(group.get_unanimous_yes_answers())
 
-print(f'Sum of Yes answers: {yes_answer_count}')
+print(f'Unanimous yes count is {unanimous_yes_answer_count}')
